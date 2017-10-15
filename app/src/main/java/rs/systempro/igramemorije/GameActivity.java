afde92491d9ff7899da2a9813696c9a3613ae087
marijatrifkovic <trifkovicmarija@hotmail.com>
2017-10-15 11:45:44 +0200

    added images

--- app/src/main/java/rs/systempro/igramemorije/GameActivity.java
+++ app/src/main/java/rs/systempro/igramemorije/GameActivity.java
@@ -2,19 +2,28 @@ package rs.systempro.igramemorije;
 
 import android.content.Intent;
 import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameActivity extends AppCompatActivity {
 
     int h;
     int w;
+
+    ArrayList<Drawable> images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +33,62 @@ public class GameActivity extends AppCompatActivity {
         Intent myIntent = getIntent();
         h= myIntent.getIntExtra("height", 4);
         w = myIntent.getIntExtra("width",3);
+        images=new ArrayList<>();
+        for(int i=0;i<(h*w)/2;i++)
+        {int resId=getResources().getIdentifier("slicica"+(i+1),"drawable",getPackageName());
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                images.add(getDrawable(resId));
+                images.add(getDrawable(resId));
+            }
+            else{images.add(getResources().getDrawable(resId));
+                images.add(getResources().getDrawable(resId));
+            }
+        }
 
+        Collections.shuffle(images);
 
+        final RelativeLayout rlRoot=(RelativeLayout) findViewById(R.id.rlRoot);
+        final GridLayout glMreza = (GridLayout) findViewById(R.id.glMreza);
+        glMreza.setColumnCount(w);
+
+        rlRoot.post(new Runnable() {
+            @Override
+            public void run() {
+                int rootH = rlRoot.getHeight();
+                int rootW = rlRoot.getWidth();
+                Toast.makeText(getApplicationContext(), rootH + "-" + rootW, Toast.LENGTH_SHORT).show();
+
+                for(int i=0;i<h*w;i++){
+                    ImageButton b = new ImageButton(getApplicationContext());
+
+                    b.setImageDrawable(images.get(i));
+                    glMreza.addView(b);
+                    GridLayout.LayoutParams param =new GridLayout.LayoutParams();
+                    if(rootH/h<rootW/w)
+                    {
+                        param.height=rootH/h-20;
+                        param.width=rootH/h-20;
+                    }
+                    else
+                    {
+                        param.height=rootW/w-20;
+                        param.width=rootW/w-20;
+                    }
+                    param.height = rootH/h-20;
+                    param.width = rootW/w-20;
+                    param.setMargins(10,10,10,10);
+                    b.setLayoutParams(param);
+                }
+            }
+        });
     }
 
-    @Override
+
+ /*  @Override
     protected void onResume() {
         super.onResume();
 
-        GridLayout glMreza = (GridLayout) findViewById(R.id.glMreza);
+        final GridLayout glMreza = (GridLayout) findViewById(R.id.glMreza);
 
         if(glMreza.getChildCount()!=0){
             return;
@@ -56,5 +112,5 @@ public class GameActivity extends AppCompatActivity {
             param.width = rootW/w;
             b.setLayoutParams(param);
         }
-    }
+    }*/
 }
